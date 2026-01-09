90abcd09dc1396c4f3f60b1d7f559bedd1c3d6a2
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2026-01-09 22:35:57 +0530

    Model: RandomForest, trees=200, depth=10

--- lab2/train.py
+++ lab2/train.py
@@ -2,16 +2,15 @@ import json
 from pathlib import Path
 import joblib
 import numpy as np
+from sklearn.linear_model import Ridge
 from ucimlrepo import fetch_ucirepo
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-
+from sklearn.ensemble import RandomForestRegressor
 # 1. Load dataset
-model = Ridge(alpha=1.0)
+
 wine = fetch_ucirepo(id=186)
 X = wine.data.features
 y = wine.data.targets.values.ravel()
@@ -29,7 +28,11 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # 4. Model
-model = Ridge(alpha=1.0)
+model = RandomForestRegressor(
+    n_estimators=200,
+    max_depth=10,
+    random_state=42
+)
 
 model.fit(X_train, y_train)
 
